aa4ccd249a6fda971f3f676d0076370bbe042ded
Rajumullapati <bhaskar1.narashimha@gmail.com>
2020-06-10 23:43:45 +0530

    Revert "Update AboutUs.js"
    This reverts commit 92dd2a437e80520df0f18c9fe8f8d986e50a2f01.

--- aameys-portal-react/src/Components/Common/AboutUs.js
+++ aameys-portal-react/src/Components/Common/AboutUs.js
@@ -13,26 +13,14 @@ export default class AboutUs extends Component {
             <HeaderCommon id='4' />
             <div style={{backgroundColor:"orange",height:"550px",opacity:"0.65"}} className="mt-10">
                 
-                <h1>Who We Are</h1>
-                <p style={{marging:"30px"}}>
+                <h1>Welcome to AAMEYS</h1>
+                <p>
                 
-                
-               
-                      <h2> African-Australian Multicultural Employment and Youth Services (AAMEYS), is a Not for Profit- Community Based Organisation (NGO/CBO), which is responding to challenges facing African Australians, in the areas of </h2>
-                       <br/> Employment 
-                        <br/>Mental Health
-                       <br/> Social Isolation
-                        <br/>Education
-                        <br/>Risk Behaviours leading to contact with the Criminal Justice System, as well as emerging issues.
-                        <br/>Racism
-                        <br/>Disproportionate levels of exclusion
-                        <br/>Low income and or institutionalised racism exacerbate the situation for most of the young people receiving services. 
-                
-                        
+                    Hi afnsdkjbch
+                     hjvcd ,zc 
+                     dbhakjcjm
                 </p>
             </div>
-
-            
             <div>
                 <Row>
                     <Col className="col-md-3" style={{marginTop:"30px",textAlign:"center",justifyContent:"center", alignContent:"center"}}>
